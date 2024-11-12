654bf4d54c839a9b679b1fa86331b76973f5aeef
rodd <saiidrodd@gmail.com>
2024-11-12 20:11:37 +0330

    Update nvim : lsp

--- .config/nvim/lua/plugins/lsp-null.lua
+++ .config/nvim/lua/plugins/lsp-null.lua
@@ -138,11 +138,11 @@ return {
 
 			-- ==> Show diagnostic windows on hover :
 			-- vim.diagnostic.config({
-			--    virtual_text = false
+			-- 	virtual_text = true,
 			-- })
 			-- -- Show line diagnostics automatically in hover window
 			-- vim.o.updatetime = 250
-			-- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+			-- vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
 			-- -- ==> Show diagnostic inline text :
 			vim.diagnostic.config({
@@ -211,279 +211,197 @@ return {
 	-- Auto-completion : cmp
 	{
 		"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-buffer",
-		"FelipeLema/cmp-async-path",
-		"saadparwaiz1/cmp_luasnip",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/cmp-calc",
-		"rafamadriz/friendly-snippets",
-	},
-	event = "InsertEnter",
-	config = function()
-		local cmp = require("cmp")
-
-		local check_backspace = function()
-			local col = vim.fn.col(".") - 1
-			return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-		end
-
-		local kind_icons = {
-			Array = " ",
-			Boolean = " ",
-			Class = " ",
-			Color = " ",
-			Constant = " ",
-			Constructor = " ",
-			Copilot = " ",
-			Enum = " ",
-			EnumMember = " ",
-			Event = " ",
-			Field = " ",
-			File = " ",
-			Folder = " ",
-			Function = " ",
-			Interface = " ",
-			Key = " ",
-			Keyword = " ",
-			Method = " ",
-			Module = " ",
-			Namespace = " ",
-			Null = " ",
-			Number = " ",
-			Object = " ",
-			Operator = " ",
-			Package = " ",
-			Property = " ",
-			Reference = " ",
-			Snippet = " ",
-			String = " ",
-			Struct = " ",
-			Text = " ",
-			TypeParameter = " ",
-			Unit = " ",
-			Value = " ",
-			Variable = " ",
-		}
-
-		local function border(hl_name)
-			return {
-				{ "╭", hl_name },
-				{ "─", hl_name },
-				{ "╮", hl_name },
-				{ "│", hl_name },
-				{ "╯", hl_name },
-				{ "─", hl_name },
-				{ "╰", hl_name },
-				{ "│", hl_name },
-			}
-		end
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body) -- For `luasnip` users.
-				end,
+		dependencies = {
+			"hrsh7th/cmp-buffer", -- source for text in buffer
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path", -- source for file system paths
+			{
+				"L3MON4D3/LuaSnip",
+				-- follow latest release.
+				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+				-- install jsregexp (optional!).
+				build = "make install_jsregexp",
 			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-				["<C-e>"] = cmp.mapping({
-					i = cmp.mapping.abort(),
-					c = cmp.mapping.close(),
-				}),
-				-- Accept currently selected item. If none selected, `select` first item.
-				-- Set `select` to `false` to only confirm explicitly selected items.
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				-- ["<Tab>"] = cmp.mapping(function(fallback)
-				-- 	if cmp.visible() then
-				-- 		cmp.select_next_item()
-				-- 	elseif luasnip.expandable() then
-				-- 		luasnip.expand()
-				-- 	elseif luasnip.expand_or_jumpable() then
-				-- 		luasnip.expand_or_jump()
-				-- 	elseif check_backspace() then
-				-- 		fallback()
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end, {
-				-- 	"i",
-				-- 	"s",
-				-- }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, {
-					"i",
-					"s",
-				}),
-			}),
-			formatting = {
-				fields = { "kind", "abbr", "menu" },
-				format = function(entry, vim_item)
-					vim_item.kind = kind_icons[vim_item.kind]
-					vim_item.menu = ({
-						nvim_lsp = "",
-						nvim_lua = "",
-						luasnip = "",
-						buffer = "",
-						path = "",
-						emoji = "",
-					})[entry.source.name]
-					return vim_item
-				end,
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "buffer" },
-				{ name = "async_path" },
-				{ name = "calc" },
-				{ name = "crates" },
-			},
-			confirm_opts = {
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = false,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = {
-					border = border("CmpDocBorder"),
-					winhighlight = "Normal:CmpDoc",
+			"saadparwaiz1/cmp_luasnip", -- for autocompletion
+			"rafamadriz/friendly-snippets", -- useful snippets
+			"onsails/lspkind.nvim", -- vs-code like pictograms
+		},
+		event = "InsertEnter",
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
+
+			-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local function border(hl_name)
+				return {
+					{ "╭", hl_name },
+					{ "─", hl_name },
+					{ "╮", hl_name },
+					{ "│", hl_name },
+					{ "╯", hl_name },
+					{ "─", hl_name },
+					{ "╰", hl_name },
+					{ "│", hl_name },
+				}
+			end
+
+			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,preview,noselect",
 				},
-				--cmp.config.window.bordered(),
-			},
-			experimental = {
-				ghost_text = true,
-			},
-			enabled = function()
-				-- Disable nvim-cmp in a telescope prompt
-				buftype = vim.api.nvim_buf_get_option(0, "buftype")
-				if buftype == "prompt" then
-					return false
-				end
-				-- Disable completion in comments
-				local context = require("cmp.config.context")
-				-- Keep command mode completion enabled when cursor is in a comment
-				if vim.api.nvim_get_mode().mode == "c" then
-					return true
-				else
-					return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-				end
-			end,
-		})
-
-		-- cmp-cmdline setup
-		cmp.setup.cmdline("/", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
-		})
-
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{
-					name = "cmdline",
-					option = {
-						ignore_cmds = { "Man", "!" },
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+					["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+					["<C-e>"] = cmp.mapping.abort(), -- close completion window
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- snippets
+					{ name = "buffer" }, -- text within current buffer
+					{ name = "path" }, -- file system paths
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = {
+						border = border("CmpDocBorder"),
+						winhighlight = "Normal:CmpDoc",
 					},
+					--cmp.config.window.bordered(),
 				},
-			}),
-		})
-	end,
+				enabled = function()
+					-- Disable nvim-cmp in a telescope prompt
+					if buftype == "prompt" then
+						return false
+					end
+					-- Disable completion in comments
+					local context = require("cmp.config.context")
+					-- Keep command mode completion enabled when cursor is in a comment
+					if vim.api.nvim_get_mode().mode == "c" then
+						return true
+					else
+						return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+					end
+				end,
+			})
+
+			-- cmp-cmdline setup
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{
+						name = "cmdline",
+						option = {
+							ignore_cmds = { "Man", "!" },
+						},
+					},
+				}),
+			})
+		end,
 	},
 	{
-		'nvim-treesitter/nvim-treesitter',
+		"nvim-treesitter/nvim-treesitter",
 		version = false, -- last release is way too old and doesn't work on Windows
-		build = ':TSUpdate',
-		event = { 'BufReadPost', 'BufNewFile' },
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
 		lazy = false,
 		dependencies = {
-		  {
-			'nvim-treesitter/nvim-treesitter-textobjects',
-			init = function()
-			  -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-			  local plugin = require('lazy.core.config').spec.plugins['nvim-treesitter']
-			  local opts = require('lazy.core.plugin').values(plugin, 'opts', false)
-			  local enabled = false
-			  if opts.textobjects then
-				for _, mod in ipairs({ 'move', 'select', 'swap', 'lsp_interop' }) do
-				  if opts.textobjects[mod] and opts.textobjects[mod].enable then
-					enabled = true
-					break
-				  end
-				end
-			  end
-			  if not enabled then
-				require('lazy.core.loader').disable_rtp_plugin('nvim-treesitter-textobjects')
-			  end
-			end,
-		  },
-		  {
-			'windwp/nvim-ts-autotag',
-		  },
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				init = function()
+					-- PERF: no need to load the plugin, if we only need its queries for mini.ai
+					local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+					local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+					local enabled = false
+					if opts.textobjects then
+						for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+							if opts.textobjects[mod] and opts.textobjects[mod].enable then
+								enabled = true
+								break
+							end
+						end
+					end
+					if not enabled then
+						require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+					end
+				end,
+			},
+			{
+				"windwp/nvim-ts-autotag",
+			},
 		},
 		keys = {
-		  { '<c-space>', desc = 'Increment selection' },
-		  { '<bs>', desc = 'Schrink selection', mode = 'x' },
+			{ "<c-space>", desc = "Increment selection" },
+			{ "<bs>", desc = "Schrink selection", mode = "x" },
 		},
 		opts = {
-		  highlight = { enable = true },
-		  indent = { enable = true, disable = { 'python' } },
-		  autotag = { enable = true },
-		  context_commentstring = { enable = true, enable_autocmd = false },
-		  ensure_installed = {
-			'bash',
-			'dockerfile',
-			'gitignore',
-			'sql',
-			'html',
-			'css',
-			'scss',
-			'json',
-			'javascript',
-			'typescript',
-			'prisma',
-			'tsx',
-			'jsdoc',
-			'lua',
-			'markdown',
-			'markdown_inline',
-			'regex',
-			'python',
-			'rust',
-			'toml',
-			'vim',
-			'vimdoc',
-			'yaml',
-		  },
-		  incremental_selection = {
-			enable = true,
-			keymaps = {
-			  init_selection = '<C-space>',
-			  node_incremental = '<C-space>',
-			  scope_incremental = '<nop>',
-			  node_decremental = '<bs>',
+			highlight = { enable = true },
+			indent = { enable = true, disable = { "python" } },
+			autotag = { enable = true },
+			context_commentstring = { enable = true, enable_autocmd = false },
+			ensure_installed = {
+				"bash",
+				"dockerfile",
+				"gitignore",
+				"sql",
+				"html",
+				"css",
+				"scss",
+				"json",
+				"javascript",
+				"typescript",
+				"prisma",
+				"tsx",
+				"jsdoc",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"regex",
+				"python",
+				"rust",
+				"toml",
+				"vim",
+				"vimdoc",
+				"yaml",
 			},
-		  },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = "<nop>",
+					node_decremental = "<bs>",
+				},
+			},
 		},
 		config = function(_, opts)
-		  require('nvim-treesitter.configs').setup(opts)
+			require("nvim-treesitter.configs").setup(opts)
 		end,
-	  }	  
+	},
 }
