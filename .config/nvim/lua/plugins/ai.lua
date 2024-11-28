81a343fabfb4a4aa7422636f7547767a0c780cdf
saiidrodd <saiidrodd@gmail.com>
2024-11-28 15:57:13 +0330

    update nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -7,7 +7,7 @@ return {
 				keymaps = {
 					accept_suggestion = "<Tab>",
 					clear_suggestion = "<C-]>",
-					accept_word = "<A-Right>",
+					accept_word = "<C-Right>",
 				},
 			})
 		end,
@@ -69,18 +69,4 @@ return {
 			},
 		},
 	},
-
-	-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> codecompanian
-	-- {
-	-- 	"olimorris/codecompanion.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 		"hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
-	-- 		"nvim-telescope/telescope.nvim", -- Optional: For using slash commands
-	-- 		{ "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
-	-- 		{ "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
-	-- 	},
-	-- 	config = true,
-	-- },
 }
