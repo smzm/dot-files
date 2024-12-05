133bc622369d50dedbfeb93656d9d49e050529cd
saiidrodd <saiidrodd@gmail.com>
2024-12-05 14:16:16 +0330

    update keymap

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -72,8 +72,8 @@ map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize splits with arrow keys
-map("n", "<M-l>", ":vertical resize +1<CR>", opts)
-map("n", "<M-h>", ":vertical resize -1<CR>", opts)
+map("n", "<M-l>", ":vertical resize -1<CR>", opts)
+map("n", "<M-h>", ":vertical resize +1<CR>", opts)
 map("n", "<M-k>", ":resize +1<CR>", opts)
 map("n", "<M-j>", ":resize -1<CR>", opts)
 
@@ -84,12 +84,12 @@ map("n", "<C-PageUp>", ":BufferLineCyclePrev<CR>", opts)
 map("n", "<C-PageDown>", ":BufferLineCycleNext<CR>", opts)
 
 -- Move lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", opts)
-map("n", "<A-k>", "<cmd>m .-2<cr>==", opts)
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", opts)
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", opts)
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", opts)
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", opts)
+map("n", "<S-j>", "<cmd>m .+1<cr>==", opts)
+map("n", "<S-k>", "<cmd>m .-2<cr>==", opts)
+map("i", "<S-j>", "<esc><cmd>m .+1<cr>==gi", opts)
+map("i", "<S-k>", "<esc><cmd>m .-2<cr>==gi", opts)
+map("v", "<S-j>", ":m '>+1<cr>gv=gv", opts)
+map("v", "<S-k>", ":m '<-2<cr>gv=gv", opts)
 
 -- Visual
 -- better paste
