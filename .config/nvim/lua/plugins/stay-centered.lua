9e71c9ff1eebea6559bfac96065b8e32237a0117
saiidrodd <saiidrodd@gmail.com>
2025-04-23 22:37:52 +0330

    update nvim

--- .config/nvim/lua/plugins/stay-centered.lua
+++ .config/nvim/lua/plugins/stay-centered.lua
@@ -1,7 +1,7 @@
 -- --  A neovim plugin to keep your cursor at the center of the screen.
--- return {
--- 	"arnamak/stay-centered.nvim",
--- 	config = function()
--- 		require("stay-centered").setup()
--- 	end,
--- }
+return {
+	-- "arnamak/stay-centered.nvim",
+	-- config = function()
+	-- 	require("stay-centered").setup()
+	-- end,
+}
