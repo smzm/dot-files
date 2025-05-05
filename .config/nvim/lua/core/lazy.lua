a82784e87c6591069c00e3a7bd1b04cf07ea551a
saiidrodd <saiidrodd@gmail.com>
2025-05-05 07:44:27 +0330

    update plugins : quarto

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -1,42 +1,41 @@
 -- Install lazy.nvim automatically
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 local opts = {
-    ui = {
-        custom_keys = { false }
-    },
-    install = {
-        colorscheme = { 'tokyonight' }
-    },
-    performance = {
-        rtp = {
-            disabled_plugins = {
-                'gzip',
-                'netrwPlugin',
-                'tarPlugin',
-                'tohtml',
-                'tutor',
-                'zipPlugin',
-                'rplugin',
-                'editorconfig',
-                'matchparen',
-                'matchit'
-            }
-        }
-    }
+	ui = {
+		custom_keys = { false },
+	},
+	install = {
+		colorscheme = { "tokyonight" },
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+				-- 'rplugin',
+				"editorconfig",
+				"matchparen",
+				"matchit",
+			},
+		},
+	},
 }
 
 -- Load the plugins and options
-require('lazy').setup('plugins', opts)
+require("lazy").setup("plugins", opts)
