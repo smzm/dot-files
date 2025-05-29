6d1bb16a0072f162284a722f8e97e5572e516943
rodd <xyzrodd@proton.me>
2025-05-29 09:05:29 +0330

    update

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -5,9 +5,9 @@ return {
 		config = function()
 			require("supermaven-nvim").setup({
 				keymaps = {
-					accept_suggestion = "<TAB>",
+					accept_suggestion = "<Tab>",
 					clear_suggestion = "<C-]>",
-					accept_word = "<A-l>",
+					accept_word = "<C-Right>",
 				},
 				-- ignore_filetypes = { md = true },
 				-- color = {
@@ -207,24 +207,54 @@ return {
 	-- 	end,
 	-- },
 
-	-- ===================== Codeium ai : to run --> :Codeium Auth
-	--     {
-	-- 	"monkoose/neocodeium",
-	-- 	event = "VeryLazy",
+	-- -- ===================== Codeium ai : to run --> :Codeium Auth
+	-- {
+	-- 	"Exafunction/windsurf.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 	},
 	-- 	config = function()
-	-- 		-- Change '<C-g>' here to any keycode you like.
-	-- 		vim.keymap.set("i", "<C-e>", function()
-	-- 			return vim.fn["codeium#Accept"]()
-	-- 		end, { expr = true, silent = true })
-	-- 		vim.keymap.set("i", "<c-n>", function()
-	-- 			return vim.fn["codeium#CycleCompletions"](1)
-	-- 		end, { expr = true, silent = true })
-	-- 		vim.keymap.set("i", "<c-p>", function()
-	-- 			return vim.fn["codeium#CycleCompletions"](-1)
-	-- 		end, { expr = true, silent = true })
-	-- 		vim.keymap.set("i", "<c-x>", function()
-	-- 			return vim.fn["codeium#Clear"]()
-	-- 		end, { expr = true, silent = true })
+	-- 		require("codeium").setup({
+	-- 			enable_cmp_source = false,
+	-- 			virtual_text = {
+	-- 				enabled = true,
+	--
+	-- 				-- These are the defaults
+	--
+	-- 				-- Set to true if you never want completions to be shown automatically.
+	-- 				manual = false,
+	-- 				-- A mapping of filetype to true or false, to enable virtual text.
+	-- 				filetypes = {},
+	-- 				-- Whether to enable virtual text of not for filetypes not specifically listed above.
+	-- 				default_filetype_enabled = true,
+	-- 				-- How long to wait (in ms) before requesting completions after typing stops.
+	-- 				idle_delay = 75,
+	-- 				-- Priority of the virtual text. This usually ensures that the completions appear on top of
+	-- 				-- other plugins that also add virtual text, such as LSP inlay hints, but can be modified if
+	-- 				-- desired.
+	-- 				virtual_text_priority = 65535,
+	-- 				-- Set to false to disable all key bindings for managing completions.
+	-- 				map_keys = true,
+	-- 				-- The key to press when hitting the accept keybinding but no completion is showing.
+	-- 				-- Defaults to \t normally or <c-n> when a popup is showing.
+	-- 				accept_fallback = nil,
+	-- 				-- Key bindings for managing completions in virtual text mode.
+	-- 				key_bindings = {
+	-- 					-- Accept the current completion.
+	-- 					accept = "<Tab>",
+	-- 					-- Accept the next word.
+	-- 					accept_word = false,
+	-- 					-- Accept the next line.
+	-- 					accept_line = false,
+	-- 					-- Clear the virtual text.
+	-- 					clear = false,
+	-- 					-- Cycle to the next completion.
+	-- 					next = "<M-]>",
+	-- 					-- Cycle to the previous completion.
+	-- 					prev = "<M-[>",
+	-- 				},
+	-- 			},
+	-- 		})
 	-- 	end,
-	-- }
+	-- },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,10 +6,10 @@ return { -- >>> LSP
 			{ "folke/neodev.nvim", opts = {} },
 			-- Automatically install LSPs to stdpath for neovim
 			{
-				"williamboman/mason.nvim",
+				"mason-org/mason.nvim",
 				opts = { ensure_installed = { "tree-sitter-cli" } },
 			},
-			"williamboman/mason-lspconfig.nvim",
+			"mason-org/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			-- Useful status updates for LSP
 			{ "j-hui/fidget.nvim", opts = {} },
