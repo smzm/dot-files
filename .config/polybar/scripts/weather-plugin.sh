23a4e7f6d37b5a5762b0e1762fdf69932ab3de2b
smr <sm@>
2025-08-02 11:24:38 +0330

    Update Polybar

--- .config/polybar/scripts/weather-plugin.sh
+++ .config/polybar/scripts/weather-plugin.sh
@@ -23,23 +23,23 @@ UNITS="metric"
 
 # Color Settings ______________________________________________________________
 
-COLOR_CLOUD="#606060"
-COLOR_THUNDER="#d3b987"
-COLOR_LIGHT_RAIN="#73cef4"
-COLOR_HEAVY_RAIN="#b3deef"
-COLOR_SNOW="#FFFFFF"
-COLOR_FOG="#606060"
-COLOR_TORNADO="#d3b987"
-COLOR_SUN="#ffc24b"
-COLOR_MOON="#FFFFFF"
-COLOR_ERR="#f43753"
-COLOR_WIND="#73cef4"
-COLOR_COLD="#b3deef"
-COLOR_HOT="#f43753"
-COLOR_NORMAL_TEMP="#FFFFFF"
+COLOR_CLOUD="#404040"
+COLOR_THUNDER="#404040"
+COLOR_LIGHT_RAIN="#404040"
+COLOR_HEAVY_RAIN="#404040"
+COLOR_SNOW="#404040"
+COLOR_FOG="#404040"
+COLOR_TORNADO="#404040"
+COLOR_SUN="#404040"
+COLOR_MOON="#404040"
+COLOR_ERR="#909090"
+COLOR_WIND="#404040"
+COLOR_COLD="#404040"
+COLOR_HOT="#404040"
+COLOR_NORMAL_TEMP="#404040"
 
 # Leave "" if you want the default polybar color
-COLOR_TEXT=""
+COLOR_TEXT="#909090"
 # Polybar settings ____________________________________________________________
 
 # Font for the weather icons
@@ -96,7 +96,6 @@ sleep 15
 #     sleep 5
 #     ONLINE=`ping -q -w 1 -c 1 google.com &>/dev/null && echo 1 || echo 0`
 # done
-        
 
 if [[ "$COLOR_TEXT" != "" ]]; then
     COLOR_TEXT_BEGIN="%{F$COLOR_TEXT}"
@@ -116,9 +115,9 @@ URL="api.openweathermap.org/data/2.5/weather?appid=$APIKEY$UNIT_URL&lang=$LANG&q
 function getData {
     ERROR=0
     # For logging purposes
-    echo " " >> "$HOME/.weather.log"
-     echo `date`" ################################" >> "$HOME/.weather.log"
-    RESPONSE=`curl -s $URL`
+    echo " " >>"$HOME/.weather.log"
+    echo $(date)" ################################" >>"$HOME/.weather.log"
+    RESPONSE=$(curl -s $URL)
     if [[ "$1" = "-d" ]]; then
         echo $RESPONSE
         echo ""
@@ -127,7 +126,7 @@ function getData {
     # echo "Response: $RESPONSE" >> "$HOME/.weather.log"
     RESPONSECODE=0
     if [[ $CODE -eq 0 ]]; then
-        RESPONSECODE=`echo $RESPONSE | jq .cod`
+        RESPONSECODE=$(echo $RESPONSE | jq .cod)
     fi
     if [[ $CODE -ne 0 ]] || [[ $RESPONSECODE -ne 200 ]]; then
         if [[ $CODE -ne 0 ]]; then
@@ -215,26 +214,26 @@ function setIcons {
         ICON="  "
     fi
     WIND=""
-    WINDFORCE=`echo "$RESPONSE" | jq .wind.speed`
+    WINDFORCE=$(echo "$RESPONSE" | jq .wind.speed)
     if [[ $KNOTS = "yes" ]]; then
         case $UNITS in
-            "imperial") 
-                # The division by one is necessary because scale works only for divisions. bc is stupid.
-                WINDFORCE=`echo "scale=$DECIMALS;$WINDFORCE * 0.8689762419 / 1" | bc`
-                ;;
-            *)
-                WINDFORCE=`echo "scale=$DECIMALS;$WINDFORCE * 1.943844 / 1" | bc`
-                ;;
+        "imperial")
+            # The division by one is necessary because scale works only for divisions. bc is stupid.
+            WINDFORCE=$(echo "scale=$DECIMALS;$WINDFORCE * 0.8689762419 / 1" | bc)
+            ;;
+        *)
+            WINDFORCE=$(echo "scale=$DECIMALS;$WINDFORCE * 1.943844 / 1" | bc)
+            ;;
         esac
     else
         if [[ $UNITS != "imperial" ]]; then
             # Conversion from m/s to km/h
-            WINDFORCE=`echo "scale=$DECIMALS;$WINDFORCE * 3.6 / 1" | bc`
+            WINDFORCE=$(echo "scale=$DECIMALS;$WINDFORCE * 3.6 / 1" | bc)
         else
-            WINDFORCE=`echo "scale=$DECIMALS;$WINDFORCE / 1" | bc`
+            WINDFORCE=$(echo "scale=$DECIMALS;$WINDFORCE / 1" | bc)
         fi
     fi
-    if [[ "$DISPLAY_WIND" = "yes" ]] && [[ `echo "$WINDFORCE >= $MIN_WIND" |bc -l` -eq 1 ]]; then
+    if [[ "$DISPLAY_WIND" = "yes" ]] && [[ $(echo "$WINDFORCE >= $MIN_WIND" | bc -l) -eq 1 ]]; then
         WIND="%{T$WEATHER_FONT_CODE}%{F$COLOR_WIND}%{F-}%{T-}"
         if [[ $DISPLAY_FORCE = "yes" ]]; then
             WIND="$WIND $COLOR_TEXT_BEGIN$WINDFORCE$COLOR_TEXT_END"
@@ -257,12 +256,12 @@ function setIcons {
     else
         TEMP_ICON="洞 "
     fi
-    
-    TEMP=`echo "$TEMP" | cut -d "." -f 1`
-    
+
+    TEMP=$(echo "$TEMP" | cut -d "." -f 1)
+
     if [[ "$TEMP" -le $COLD_TEMP ]]; then
         TEMP="%{F$COLOR_COLD}%{T$TEMP_FONT_CODE}%{T-}%{F-} $COLOR_TEXT_BEGIN$TEMP%{T$TEMP_FONT_CODE}$TEMP_ICON%{T-}$COLOR_TEXT_END"
-    elif [[ `echo "$TEMP >= $HOT_TEMP" | bc` -eq 1 ]]; then
+    elif [[ $(echo "$TEMP >= $HOT_TEMP" | bc) -eq 1 ]]; then
         TEMP="%{F$COLOR_HOT}%{T$TEMP_FONT_CODE}%{T-}%{F-} $COLOR_TEXT_BEGIN$TEMP%{T$TEMP_FONT_CODE}$TEMP_ICON%{T-}$COLOR_TEXT_END"
     else
         TEMP="%{F$COLOR_NORMAL_TEMP}%{T$TEMP_FONT_CODE}%{T-}%{F-} $COLOR_TEXT_BEGIN$TEMP%{T$TEMP_FONT_CODE}$TEMP_ICON%{T-}$COLOR_TEXT_END"
@@ -277,21 +276,21 @@ function outputCompact {
 
 getData $1
 if [[ $ERROR -eq 0 ]]; then
-    MAIN=`echo $RESPONSE | jq .weather[0].main`
-    WID=`echo $RESPONSE | jq .weather[0].id`
-    DESC=`echo $RESPONSE | jq .weather[0].description`
-    SUNRISE=`echo $RESPONSE | jq .sys.sunrise`
-    SUNSET=`echo $RESPONSE | jq .sys.sunset`
-    DATE=`date +%s`
+    MAIN=$(echo $RESPONSE | jq .weather[0].main)
+    WID=$(echo $RESPONSE | jq .weather[0].id)
+    DESC=$(echo $RESPONSE | jq .weather[0].description)
+    SUNRISE=$(echo $RESPONSE | jq .sys.sunrise)
+    SUNSET=$(echo $RESPONSE | jq .sys.sunset)
+    DATE=$(date +%s)
     WIND=""
-    TEMP=`echo $RESPONSE | jq .main.temp`
+    TEMP=$(echo $RESPONSE | jq .main.temp)
     if [ $DISPLAY_LABEL = "yes" ]; then
-        DESCRIPTION=`echo "$RESPONSE" | jq .weather[0].description | tr -d '"' | sed 's/.*/\L&/; s/[a-z]*/\u&/g'`" "
+        DESCRIPTION=$(echo "$RESPONSE" | jq .weather[0].description | tr -d '"' | sed 's/.*/\L&/; s/[a-z]*/\u&/g')" "
     else
         DESCRIPTION=""
     fi
-    PRESSURE=`echo $RESPONSE | jq .main.pressure`
-    HUMIDITY=`echo $RESPONSE | jq .main.humidity`
+    PRESSURE=$(echo $RESPONSE | jq .main.pressure)
+    HUMIDITY=$(echo $RESPONSE | jq .main.humidity)
     setIcons
     outputCompact
 else
