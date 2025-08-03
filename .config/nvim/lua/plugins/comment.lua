757a0ea7faba0a3ff84a8f60a3e23de217761642
smr <sm@>
2025-08-03 11:08:18 +0330

    -m

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -25,6 +25,8 @@ return {
 			-- set keymaps
 			local keymap = vim.keymap -- for conciseness
 
+			keymap.set("n", "<leader>=", "<cmd>TodoTelescope<CR>", { desc = "Previous todo comment" })
+
 			keymap.set("n", "]t", function()
 				todo_comments.jump_next()
 			end, { desc = "Next todo comment" })
