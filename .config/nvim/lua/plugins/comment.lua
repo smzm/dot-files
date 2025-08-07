46e7db66376b5d6d0c58fbce86170085bf2d9aee
smr <sm@>
2025-08-07 12:17:09 +0330

    -m

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -25,7 +25,12 @@ return {
 			-- set keymaps
 			local keymap = vim.keymap -- for conciseness
 
-			keymap.set("n", "<leader>=", "<cmd>TodoTelescope<CR>", { desc = "Previous todo comment" })
+			keymap.set(
+				"n",
+				"<leader>=",
+				"<cmd>TodoTelescope  keywords=TODO,FIX,WARNING,HACK,TEST<CR>",
+				{ desc = "Previous todo comment" }
+			)
 
 			keymap.set("n", "]t", function()
 				todo_comments.jump_next()
@@ -46,6 +51,20 @@ return {
 				},
 			})
 		end,
+		keywords = {
+			FIX = {
+				icon = " ", -- icon used for the sign, and in search results
+				color = "error", -- can be a hex color, or a named color (see below)
+				alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR" }, -- a set of other keywords that all map to this FIX keywords
+				-- signs = false, -- configure signs for some keywords individually
+			},
+			TODO = { icon = " ", color = "info" },
+			HACK = { icon = " ", color = "warning" },
+			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+			PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+			NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+			TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+		},
 	},
 	-- TODO :
 	-- NOTE :
