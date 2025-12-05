251ca2980184f1ac46f7ff0c538bcdc8e48f3252
SMZM <saiidrodd@gmail.com>
2025-12-05 12:58:43 +0330

    -m

--- .config/nvim/lua/plugins/csvview.lua
+++ .config/nvim/lua/plugins/csvview.lua
@@ -4,16 +4,24 @@ return {
 		parser = {
 			comments = { "#", "//" },
 			delimiter = {
-				default = ",",
 				ft = {
-					tsv = "\t",
+					csv = ",", -- Always use comma for .csv files
+					tsv = "\t", -- Always use tab for .tsv files
+				},
+				fallbacks = { -- Try these delimiters in order for other files
+					",", -- Comma (most common)
+					"\t", -- Tab
+					";", -- Semicolon
+					"|", -- Pipe
+					":", -- Colon
+					" ", -- Space
 				},
 			},
 		},
 		view = {
 			min_column_width = 7,
 			spacing = 2,
-			display_mode = "highlight", -- "highlight", "border"
+			display_mode = "highlight",
 			header_lnum = 1,
 			sticky_header = {
 				enabled = true,
@@ -21,18 +29,26 @@ return {
 			},
 		},
 		keymaps = {
-			-- Text objects for selecting fields
 			textobject_field_inner = { "if", mode = { "o", "x" } },
 			textobject_field_outer = { "af", mode = { "o", "x" } },
-			-- Excel-like navigation:
-			-- Use <Tab> and <S-Tab> to move horizontally between fields.
-			-- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-			-- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+
 			jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
 			jump_prev_field_end = { "<S-tab>", mode = { "n", "v" } },
 			jump_next_row = { "<Enter>", mode = { "n", "v" } },
 			jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
 		},
 	},
+
 	cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+	ft = { "csv", "tsv" }, -- Load plugin for csv/tsv filetypes
+	init = function()
+		local augroup = vim.api.nvim_create_augroup("csvview_autoenable", { clear = true })
+		vim.api.nvim_create_autocmd({ "FileType" }, {
+			pattern = { "csv", "tsv" },
+			group = augroup,
+			callback = function()
+				vim.cmd("CsvViewEnable")
+			end,
+		})
+	end,
 }
