84c5cdf7f665b9992fec6a79c9f3922a66362bd1
smzm <saiidrodd@gmail.com>
2025-12-20 15:16:17 +0330

    -m

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,26 +7,26 @@ return {
 		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 		lazy = false,
 		dependencies = {
-			{
-				"nvim-treesitter/nvim-treesitter-textobjects",
-				init = function()
-					-- PERF: no need to load the plugin, if we only need its queries for mini.ai
-					local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-					local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-					local enabled = false
-					if opts.textobjects then
-						for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-							if opts.textobjects[mod] and opts.textobjects[mod].enable then
-								enabled = true
-								break
-							end
-						end
-					end
-					if not enabled then
-						require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-					end
-				end,
-			},
+			-- {
+			-- 	"nvim-treesitter/nvim-treesitter-textobjects",
+			-- 	init = function()
+			-- 		-- PERF: no need to load the plugin, if we only need its queries for mini.ai
+			-- 		local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+			-- 		local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+			-- 		local enabled = false
+			-- 		if opts.textobjects then
+			-- 			for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+			-- 				if opts.textobjects[mod] and opts.textobjects[mod].enable then
+			-- 					enabled = true
+			-- 					break
+			-- 				end
+			-- 			end
+			-- 		end
+			-- 		if not enabled then
+			-- 			require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+			-- 		end
+			-- 	end,
+			-- },
 			{
 				"windwp/nvim-ts-autotag",
 				event = { "BufReadPre", "BufNewFile" },
@@ -225,7 +225,7 @@ return {
 			},
 		},
 		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
+			require("nvim-treesitter").setup(opts)
 		end,
 	},
 	{
