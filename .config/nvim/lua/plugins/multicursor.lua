1c834111898efd9e08950898792b4b8793c7497a
smzm <saiidrodd@gmail.com>
2025-12-21 19:15:05 +0330

    -m

--- .config/nvim/lua/plugins/multicursor.lua
+++ .config/nvim/lua/plugins/multicursor.lua
@@ -8,10 +8,10 @@ return {
 		local set = vim.keymap.set
 
 		-- Add or skip cursor above/below the main cursor.
-		set({ "n", "x" }, "<up>", function()
+		set({ "n", "x" }, "<C-up>", function()
 			mc.lineAddCursor(-1)
 		end)
-		set({ "n", "x" }, "<down>", function()
+		set({ "n", "x" }, "<C-down>", function()
 			mc.lineAddCursor(1)
 		end)
 		set({ "n", "x" }, "<leader><up>", function()
