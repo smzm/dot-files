f1f6936a3e4fe7079ece7b62403bd440e3fde885
SMR <saiidrodd@gmail.com>
2026-02-08 13:47:58 +0330

    Update GeoIpLocation

--- .config/polybar/scripts/ipGeoLocation.sh
+++ .config/polybar/scripts/ipGeoLocation.sh
@@ -3,7 +3,7 @@ sleep 15
 
 IP=`curl -w "\n" -s https://api.ipify.org`
 if [[ ! $IP == "" ]] ;then
-    country=`geoiplookup "$IP" | awk 'BEGIN{FS=":"}; {print $2}' | awk 'BEGIN{FS=","}; {gsub(/[[:blank:]]/,""); print $1}'`
+    country=`geoiplookup "$IP" | awk 'BEGIN{FS=":"}; {print $2}' | awk 'BEGIN{FS=","}; {gsub(/[[:blank:]]/,""); print $2}'`
     # flag=`jq '.[] | select(.iso=='"$country"') .flag' ~/country.json 2> /dev/null` 
     # clean_flag=`echo "$flag" | awk '{gsub(/"/, "", $0) ; print $0}'`
     echo "$country"
