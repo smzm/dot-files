39ed914053278e2f97cf55d9fc24bbc5166a6cbd
SAEED MZM RODD <39596095+smzm@users.noreply.github.com>
2026-07-01 12:22:25 +0330

    Update yazi

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -1,5 +1,5 @@
 return {
-	"DreamMaoMao/yazi.nvim",
+	"mikavilpas/yazi.nvim",
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
 		"nvim-lua/plenary.nvim",
@@ -7,5 +7,9 @@ return {
 
 	keys = {
 		{ "<Tab>", "<cmd>Yazi<CR>", desc = "Toggle Yazi", mode = "n" },
+	},
+	opts = {
+		open_for_directories = false, -- change to true if you want Yazi for dirs too
+		-- other options...
 	},
 }
