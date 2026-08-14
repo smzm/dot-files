78f52ab8dc6bdda768dc6969bb2e576570471a8d
smzm <smzm@github.com>
2026-08-14 13:50:53 +0330

    Update hyprland

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -49,7 +49,7 @@ local colorPicker = "hyprpicker"
 --
 hl.on("hyprland.start", function()
 	hl.exec_cmd("waybar")
-	hl.exec_cmd("hyprpaper")
+	-- hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("hyprpolkitagent")
 	hl.exec_cmd("nm-applet")
@@ -215,6 +215,7 @@ hl.config({
 	misc = {
 		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
+		disable_splash_rendering = true,
 	},
 })
 
